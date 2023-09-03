fc12fa72d4098fb0759edcaa3e9a3b63617ef6f9
league <league@imac-39.attlocal.net>
2023-09-03 11:26:37 -0700

    Checkpoint

--- src/Checkpoint.java
+++ src/Checkpoint.java
@@ -3,17 +3,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Checkpoint {
 	public static void main(String[] args) {
 		/*
-		 * The auto-mgp.data file contains the miles-per-gallon data on various different types of cars.
-		 * Each line of the file provides the data for one CarMPGEntry object.
-		 * (source: http://archive.ics.uci.edu/ml/datasets)
+		 * The auto-mgp.data file contains the miles-per-gallon data on various
+		 * different types of cars. Each line of the file provides the data for one
+		 * CarMPGEntry object. (source: http://archive.ics.uci.edu/ml/datasets)
 		 * 
-		 * Use the method provided to get an ArrayList of CarMPGEntry objects. Convert the ArrayList into a Stream.
-		 * Using streams, perform the following:
+		 * Use the method provided to get an ArrayList of CarMPGEntry objects. Convert
+		 * the ArrayList into a Stream. Using streams, perform the following:
 		 * 
 		 * 1. Print the entire list.
 		 * 
@@ -29,68 +32,105 @@ public class Checkpoint {
 		 * 
 		 * 5. Print only the cars with "toyota" in the name.
 		 */
-		
-		
+		ArrayList<CarMPGEntry> list = readCarMPGEntryDataFromFile();
+		Stream<CarMPGEntry> stream = list.stream();
+		// 1
+		stream.forEach((CarMPGEntry) -> System.out
+				.println(CarMPGEntry.mpg + " " + CarMPGEntry.cylinders + " " + CarMPGEntry.displacement + " "
+						+ CarMPGEntry.horsePower + " " + CarMPGEntry.weight + " " + CarMPGEntry.acceleration + " "
+						+ CarMPGEntry.modelYear + "  " + CarMPGEntry.origin + " " + CarMPGEntry.carName));
+		// 2
+	//	stream.forEach((CarMPGEntry) -> System.out.println(CarMPGEntry.mpg));
+		// 3
+		list.stream().sorted((CarMPGEntry1, CarMPGEntry2) -> CarMPGEntry1.carName.compareTo(CarMPGEntry2.carName))
+				.forEach((CarMPGEntry) -> System.out.println(CarMPGEntry.carName));
+		// 4
+		list.stream().filter((CarMPGEntry) -> CarMPGEntry.cylinders != 8)
+				.forEach((CarMPGEntry) -> System.out
+						.println(CarMPGEntry.mpg + " " + CarMPGEntry.cylinders + " " + CarMPGEntry.displacement + " "
+								+ CarMPGEntry.horsePower + " " + CarMPGEntry.weight + " " + CarMPGEntry.acceleration
+								+ " " + CarMPGEntry.modelYear + "  " + CarMPGEntry.origin + " " + CarMPGEntry.carName));
+		// 5
+		list.stream().filter((CarMPGEntry) -> CarMPGEntry.carName.contains("toyota"))
+				.forEach((CarMPGEntry) -> System.out.println(CarMPGEntry.carName));
 	}
 	
-	public static ArrayList<CarMPGEntry> readCarMPGEntryDataFromFile(){
+	String [] strArray = new String [4];
+	Stream <String> strStream = Arrays.stream(strArray);
+	
+	ArrayList <Integer> intList = new ArrayList <Integer>();
+	Stream <Integer> intStream = intList.stream();
+	
+	
+
+	public static ArrayList<CarMPGEntry> readCarMPGEntryDataFromFile() {
 		ArrayList<CarMPGEntry> carList = new ArrayList<CarMPGEntry>();
-		
+
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("auto-mpg.data"));
-			
+
 			String line = br.readLine();
-			while(line != null) {
+			while (line != null) {
 				String entry = "";
 				ArrayList<String> splitEntry = new ArrayList<String>();
-				for(int i = 0; i < line.length(); i++) {
+				for (int i = 0; i < line.length(); i++) {
 					char c = line.charAt(i);
-					if(c == ' ' || c == '\t') {
-						if(entry.length() > 0) {
+					if (c == ' ' || c == '\t') {
+						if (entry.length() > 0) {
 							splitEntry.add(entry);
 							entry = "";
 						}
-					}else if(c =='\"') {
+					} else if (c == '\"') {
 						i++;
 						c = line.charAt(i);
-						while(c != '\"') {
+						while (c != '\"') {
 							entry += c;
 							c = line.charAt(++i);
 						}
 						splitEntry.add(entry);
-					}else {
+					} else {
 						entry += c;
 					}
 				}
-				
+
 				CarMPGEntry cmpg = new CarMPGEntry();
-				
+
 				cmpg.mpg = Float.parseFloat(splitEntry.get(0));
 				cmpg.cylinders = Integer.parseInt(splitEntry.get(1));
 				cmpg.displacement = Float.parseFloat(splitEntry.get(2));
 				try {
 					cmpg.horsePower = Float.parseFloat(splitEntry.get(3));
-					
-				}catch(NumberFormatException e) {
+
+				} catch (NumberFormatException e) {
 					cmpg.horsePower = -1;
 				}
 				cmpg.weight = Float.parseFloat(splitEntry.get(4));
 				cmpg.acceleration = Float.parseFloat(splitEntry.get(5));
-				cmpg.modelYear  = Integer.parseInt(splitEntry.get(6));
+				cmpg.modelYear = Integer.parseInt(splitEntry.get(6));
 				cmpg.origin = Integer.parseInt(splitEntry.get(7));
 				cmpg.carName = splitEntry.get(8);
 				carList.add(cmpg);
 
 				line = br.readLine();
 			}
-			
+
 			br.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return carList;
 	}
 }
+
+
+
+//Stream <String> s = 
+
+
+
+
+
+
